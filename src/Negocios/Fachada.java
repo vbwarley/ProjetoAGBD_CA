1b0e3a2c825a6af5aa6b7afa4b13aa6ac1b31a76
warley vital <vitalwarley@gmail.com>
2014-02-01 22:23:14 -0300

    Implementação das assinaturas dos métodos do caso de uso Manter Curso.
    Ainda falta escopo.

--- src/Negocios/Fachada.java
+++ src/Negocios/Fachada.java
@@ -1,5 +1,42 @@
 package Negocios;
 
-public class Fachada {
+import java.util.ArrayList;
 
+public class Fachada {
+	private static Fachada instance = new Fachada();
+	
+	public Fachada() {
+		
+	}
+	
+	public static Fachada getInstance() {
+		return instance;
+	}
+
+	public void cadastrarCurso(String nome, String descricao, Double valor,
+			Integer limite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean consultarCurso(Integer codigo) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public void alterarCurso(Integer codigo, String nome, String descricao,
+			Double valor, Integer limite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String consultarCursos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void excluirCurso(Integer codigo) {
+		// TODO Auto-generated method stub
+		
+	}
 }
